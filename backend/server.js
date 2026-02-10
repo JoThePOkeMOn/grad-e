da956b4247c65b7418d63763ca7b3b4a7275efa2
Sebastian Abraham <sebastianabraham2006@gmail.com>
2026-02-11 00:09:11 +0530

    basic backend server

---
+++ backend/server.js
@@ -0,0 +1,18 @@
+// Basic Express.js server boilerplate
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Server is running!");
+});
+
+// Add your API routes here
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
